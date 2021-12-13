3abff68557503db00cc29c7975116f45606d16c6
Czechball <13371865+Czechball@users.noreply.github.com>
2021-12-13 05:39:41 +0100

    implemented close, open, blink

--- dragon_eyes_base/dragon_eyes_base.ino
+++ dragon_eyes_base/dragon_eyes_base.ino
@@ -9,8 +9,23 @@
 #define SCREEN_ADDRESS	0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-int eyeDir;
-String eyeValue = "15";
+// default iris values
+
+int eyeIrisSizex = 32;
+int eyeIrisSizey = 32;
+int eyeIrisPosx = display.width()/2;
+int eyeIrisPosy = display.height()/2;
+
+// default eyesocket values
+
+int eyeSocketSizex = 64;
+int eyeSocketSizey = 64;
+int eyeSocketPosx = display.width()/2;
+int eyeSocketPosy = display.height()/2;
+
+// default animation speeds
+
+int lidSpeed = 8;
 
 void setup() {
   Serial.begin(115200);
@@ -18,25 +33,86 @@ void setup() {
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
   }
-  display.display();
+
+  eyesOpen(lidSpeed, eyeSocketSizey);
+
+  eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, eyeSocketSizey, eyeIrisPosx, eyeIrisPosy, eyeIrisSizex, eyeIrisSizey);
 }
 
 void loop() {
-  eyes();
+  delay(500);
+  eyesBlink(300);
+  delay(1000);
+  eyesClose(lidSpeed, 1);
+  delay(1000);
+  eyesOpen(lidSpeed, eyeSocketSizey);
 }
 
-void eyes() {
+void eyes(int eyeSocketPosx, int eyeSocketPosy, int eyeSocketSizex, int eyeSocketSizey, int eyeIrisPosx, int eyeIrisPosy, int eyeIrisSizex, int eyeIrisSizey) {
   display.clearDisplay();
-  display.drawCircle(display.width()/2, display.height()/2, 30, SSD1306_WHITE);
-  display.fillCircle(display.width()/2+eyeDir, display.height()/2, eyeValue.toInt(), SSD1306_WHITE);
+  drawSocket(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, eyeSocketSizey);
+  drawIris(eyeIrisPosx, eyeIrisPosy, eyeIrisSizex, eyeIrisSizey);
   display.display();
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    eyeValue = Serial.readString();
-    // say what you got:
-    Serial.print("Eye value set to ");
-    Serial.println(eyeValue);
+}
+
+void drawSocket(int posx, int posy, int sizex, int sizey) {
+  // set eyesocket, position center
+  display.drawRoundRect(posx-sizex/2, posy-sizey/2, sizex, sizey, sizex*2, SSD1306_WHITE);
+}
+
+void drawIris(int posx, int posy, int sizex, int sizey) {
+  // set iris, position center + eyeDir
+  display.fillRoundRect(posx-sizex/2, posy-sizey/2, sizex, sizey, sizex*2, SSD1306_WHITE);
+}
+
+void eyesOpen(int increment, int finalSize) {
+  display.clearDisplay();
+  eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, 1, eyeIrisPosx, eyeIrisPosy, 0, 0);
+  display.display();
+  delay(500);
+  for (int i = 1; i < finalSize; i = i + increment)
+    {
+      if (i <= eyeIrisSizey)
+    {
+    eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, i, eyeIrisPosx, eyeIrisPosy, 0, 0);
   }
+  else
+    {
+      eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, i, eyeIrisPosx, eyeIrisPosy, eyeIrisSizex, eyeIrisSizey);
+    }
+  display.display();
+  }
+  eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, finalSize, eyeIrisPosx, eyeIrisPosy, eyeIrisSizex, eyeIrisSizey);
+  display.display();
+}
+
+void eyesClose(int increment, int finalSize) {
+  display.clearDisplay();
+  eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, eyeSocketSizey, eyeIrisPosx, eyeIrisPosy, eyeIrisSizex, eyeIrisSizey);
+  display.display();
+  delay(500);
+  for (int i = eyeSocketSizey; i > 1; i = i - increment)
+    {
+      if (i >= eyeIrisSizey)
+    {
+    eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, i, eyeIrisPosx, eyeIrisPosy, eyeIrisSizex, eyeIrisSizey);
+  }
+  else
+    {
+      eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, i, eyeIrisPosx, eyeIrisPosy, 0, 0);
+    }
+  display.display();
+  }
+  eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, finalSize, eyeIrisPosx, eyeIrisPosy, 0, 0);
+  display.display();
+}
+
+void eyesBlink(int duration) {
+  display.clearDisplay();
+  eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, 4, eyeIrisPosx, eyeIrisPosy, 0, 0);
+  display.display();
+  delay(duration);
+  eyes(eyeSocketPosx, eyeSocketPosy, eyeSocketSizex, eyeSocketSizey, eyeIrisPosx, eyeIrisPosy, eyeIrisSizex, eyeIrisSizey);
+  display.display();
 }
